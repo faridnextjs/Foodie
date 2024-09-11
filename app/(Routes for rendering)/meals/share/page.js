ed71a930572c75f845e97e406720a4443138006d
Illusion <17003685+faridnextjs@users.noreply.github.com>
2024-09-11 13:14:18 +0200

    added ImagePicker as CSR

--- app/(Routes for rendering)/meals/share/page.js
+++ app/(Routes for rendering)/meals/share/page.js
@@ -1,19 +1,49 @@
-import Link from 'next/link';
+import classes from "./page.module.css";
 
-export default function Share() {
-  console.dir('Rendering Share');
+export default function ShareMealPage() {
   return (
     <>
-      <h1>⚠️ Welcome to the Share Section! ⚠️</h1>
-      <p>
-        <Link href={'/meals/share/postShare-1'}>Share Section 1</Link>
-      </p>
-      <p>
-        <Link href={'/meals/share/postShare-2'}>Share Section 2</Link>
-      </p>
-      <p>
-        <Link href={'/meals/share/postShare-3'}>Share Section 3</Link>
-      </p>
+      <header className={classes.header}>
+        <h1>
+          Share your <span className={classes.highlight}>favorite meal</span>
+        </h1>
+        <p>Or any other meal you feel needs sharing!</p>
+      </header>
+      <main className={classes.main}>
+        <form className={classes.form}>
+          <div className={classes.row}>
+            <p>
+              <label htmlFor="name">Your name</label>
+              <input type="text" id="name" name="name" required />
+            </p>
+            <p>
+              <label htmlFor="email">Your email</label>
+              <input type="email" id="email" name="email" required />
+            </p>
+          </div>
+          <p>
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" name="title" required />
+          </p>
+          <p>
+            <label htmlFor="summary">Short Summary</label>
+            <input type="text" id="summary" name="summary" required />
+          </p>
+          <p>
+            <label htmlFor="instructions">Instructions</label>
+            <textarea
+              id="instructions"
+              name="instructions"
+              rows="10"
+              required
+            ></textarea>
+          </p>
+          IMAGE PICKER
+          <p className={classes.actions}>
+            <button type="submit">Share Meal</button>
+          </p>
+        </form>
+      </main>
     </>
   );
 }
